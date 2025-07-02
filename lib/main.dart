8b5eecdf2c3324eba215b82e459e50d8732ad156
Dong Phan <dongphanit@gmail.com>
2025-07-02 15:53:14 +0700

    test notifi

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,12 @@ import 'package:habits/habit.dart';
 import 'package:habits/home_page.dart';
 import 'package:habits/setting_schedule_screen.dart';
 
+import 'package:timezone/timezone.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   MobileAds.instance.initialize();
-
-
+  
   const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
   const DarwinInitializationSettings initializationSettingsIOS = DarwinInitializationSettings();
   const InitializationSettings initializationSettings = InitializationSettings(
@@ -17,7 +18,41 @@ Future<void> main() async {
     iOS: initializationSettingsIOS,
   );
   await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  // cách test
+  flutterLocalNotificationsPlugin
+      .show(0, 'Test Notification', 'This is a test notification', const NotificationDetails(
+        android: AndroidNotificationDetails(
+          'your_channel_id',
+          'your_channel_name',
+          channelDescription: 'your_channel_description',
+          importance: Importance.max,
+          priority: Priority.high,
+          showWhen: false,
+        ),
+        iOS: DarwinNotificationDetails(),
+      ));
 
+
+  final now = DateTime.now();
+  await flutterLocalNotificationsPlugin.zonedSchedule(
+    0,
+    'Nhắc nhở Nhắc nhở',
+    'Đây là một thông báo nhắc nhở',
+    TZDateTime.from(now, local).add(const Duration(seconds: 5)), // Thời gian nhắc nhở
+    const NotificationDetails(
+      android: AndroidNotificationDetails(
+        'habit_channel',
+        'Habit Reminders',
+        channelDescription: 'Reminders to complete your habits',
+        importance: Importance.max,
+        priority: Priority.high,
+      ),
+      iOS: DarwinNotificationDetails(),
+    ),
+    androidAllowWhileIdle: true,
+    uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime,
+    matchDateTimeComponents: DateTimeComponents.time,
+  );
   runApp(HabitTrackerApp());
 }
 
